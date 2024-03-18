e4cf8227303f15f2cb02843db1fa35fde6afa5f5
Chartier_Marius <marius.chartierlegoff@etudiant.univ-rennes1.fr>
2024-03-18 17:19:42 +0100

    reine_symetrie suite2

--- reine_symetrie.py
+++ reine_symetrie.py
@@ -42,29 +42,36 @@ def is_valid(queens: list) -> bool:
                 return False
     return True
 
+#
+
 
 [
-    [],
-    [],
-    [],
-    []
+    [0, 0, 3, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 2, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 2, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 3, 0, 0]
 ]
 
 
 def main():
     affiche_echiquier(echiquier)
     queens = []  # position des reines
-    # demande de poser une reine
-    print("donne la ligne")
-    x = int(input())
-    print("donne la colonne")
-    y = int(input())
-    queens.append([x, y])
-    queens.append([N-1-x, N-1-y])
+
+    # # demande de poser une reine
+    # print("donne la ligne")
+    # x = int(input())
+    # print("donne la colonne")
+    # y = int(input())
+    # queens.append([x, y])
+    # queens.append([N-1-x, N-1-y])
 
     indice_ligne = 0
     indice_colonne = 0
-    for indice_colonne in range(4):
+    for indice_colonne in range(N/2):
         modif = False
         indice_ligne = 0
 
@@ -87,7 +94,72 @@ def main():
         echiquier[a][b] = 1
 
 
+def main():
+    affiche_echiquier(echiquier)
+    queens = []  # position des reines
+
+    # # demande de poser une reine
+    # print("donne la ligne")
+    # x = int(input())
+    # print("donne la colonne")
+    # y = int(input())
+    # queens.append([x, y])
+    # queens.append([N-1-x, N-1-y])
+
+    indice_ligne = 0
+    indice_colonne = 0
+    for indice_colonne in range(N/2):
+        modif = False
+        indice_ligne = 0
+
+        print(indice_colonne)
+        while indice_ligne < N and modif == False:
+
+            queens.append([indice_ligne, indice_colonne])
+            queens.append([N-indice_ligne-1, N-indice_colonne-1])
+            if is_valid(queens):
+                modif = True
+
+            else:
+                queens = queens[:-2]
+                indice_ligne += 1
+            print(queens)
+    for i in range(N):
+
+        a = queens[i][0]
+        b = queens[i][1]
+        echiquier[a][b] = 1
+
+
+def main2():
+    affiche_echiquier(echiquier)
+    queens = []  # position des reines
+
+    indice_ligne = 0
+    indice_colonne = 0
+    while not is_valid(queens) and len(queens) < N:
+
+        indice_ligne = 0
+        while not is_valid(queens) and indice_ligne < N:
+            queens.append[indice_ligne][indice_colonne]
+            queens.append[N-1-indice_ligne][N-1-indice_colonne]
+            if not is_valid(queens):
+                queens = queens[:-2]
+                indice_ligne += 1
+
+        if indice_ligne >= N:
+            indice_colonne -= 1
+        else:
+            indice_colonne += 1
+
+    for i in range(N):
+
+        a = queens[i][0]
+        b = queens[i][1]
+        echiquier[a][b] = 1
+
+
 print("")
 affiche_echiquier(echiquier)
 
-main()
+main2()
