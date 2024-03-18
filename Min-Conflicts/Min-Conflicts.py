c42c73d0903e8a0549d3a2c3e5f1913a66dce4c6
VitoVitae <93656950+VitoVitae@users.noreply.github.com>
2024-03-18 16:27:59 +0100

    creation de fonction place et initialisation de l'algo glouton

--- Min-Conflicts/Min-Conflicts.py
+++ Min-Conflicts/Min-Conflicts.py
@@ -1,17 +1,5 @@
 from colorama import Fore, Back, Style
 from random import *
-n=8
-
-board = [[0 for i in range(n)] for i in range(n)]
-
-listeR = [randint(0,7)]+[None for i in range(n-1)]
-
-
-print(listeR)
-
-for i in range(0,n):
-    if listeR[i] is not None:
-        board[listeR[i]][i]=8
 
 def afficher(echequier):
     taille = len(echequier)
@@ -24,17 +12,17 @@ def afficher(echequier):
                 print (str(j) + "  ", end ="")
         print()
 
-afficher(board)
 
 def conflicts(listeR):
     #fonction renvoi tableau des conflits
     board = [[0 for i in range(n)] for i in range(n)]
     for ind in range(len(listeR)):
         r=listeR[ind]
-        print(r)
         if r!= None:
             for i in range(n):
-                board[r][i]+=1
+                # la case ou est mise la reine n'est pas en conflit
+                if (i!=ind):
+                    board[r][i]+=1
 
             #diagonale vers le bas
             for i in range(n):
@@ -51,17 +39,48 @@ def conflicts(listeR):
    
     return board
 
+# fonction pour placer une reine dans la case d'une colonne avec le moins de conflits
+# fonction renvoie la ligne du placement de la reine 
+# prend en paramètre l'indice de la colonne et le tableau des conflits
 def place(indice, board):
     taille = len(board)
     min = 999
-    indiceMin = None
+    indiceMin = []
     for i in range(taille):
         if board[i][indice] < min:
             min = board[i][indice]
-            indiceMin = i
+            indiceMin=[]
+            indiceMin.append(i)
+        elif board[i][indice] ==  min:
+            indiceMin.append(i)
+    return choice(indiceMin)
 
 
 
+n=8
+
+board = [[0 for i in range(n)] for i in range(n)]
+
+# de 0 à 7
+listeR = [randint(0,7)]+[None for i in range(n-1)]
 
 
-afficher(conflicts(listeR))
+print(listeR)
+
+
+#initialisation
+confBoard= conflicts(listeR)
+for i in range(1,8):
+    res = place(i, confBoard)
+    listeR[i]=res
+    confBoard= conflicts(listeR)
+print(listeR)
+afficher(confBoard)
+
+#place les reines sur un echequier normal
+for i in range(0,n):
+    if listeR[i] is not None:
+        board[listeR[i]][i]=8
+
+print()
+afficher(board)
